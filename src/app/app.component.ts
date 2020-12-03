5b018ddeedcfc32d084b9934d41af841e3545412
Tahmid <tahmidfarhan12@gmail.com>
2020-12-03 22:10:25 +0600

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {WikipediaService} from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'wsearch';
+
+  pages =[] ;
+
+  constructor(private wikipedia : WikipediaService){
+
+  }
+
+  //https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=space
+
+  onTerm(term : string ){
+       this.wikipedia.search(term).subscribe( (response : any) =>{
+
+      this.pages = response.query.search ;
+
+    }) ;
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { HttpClientModule } from '@angular/common/http' ;
+
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    PageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , HttpClientModule , CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
